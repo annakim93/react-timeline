64ed363d31d851ebfbc656a4dc9a7f10c48e9ea1
Anna Kim <anna.kim93@gmail.com>
2020-12-22 01:04:30 -0500

    nested TimelineEvent component in Timeline; passed tests

--- src/App.js
+++ src/App.js
@@ -3,31 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
-import TimelineEvent from './components/TimelineEvent';
+// import TimelineEvent from './components/TimelineEvent';
 
 function App() {
   // console.log(timelineData);
 
-  const timelineEventData = {
-    // person: timelineData.events[0].person,
-    // status: timelineData.events[0].status,
-    // timeStamp: timelineData.events[0].timeStamp
-    person: 'Ada Lovelace',
-    status: 'The Original programmer... old-school style, on PAPER.',
-    timeStamp: '2018-05-18T22:12:03Z'
-  }
+  // const timelineEventData = {
+  //   person: timelineData.events[0].person,
+  //   status: timelineData.events[0].status,
+  //   timeStamp: timelineData.events[0].timeStamp
+  // }
 
-  // Customize the code below
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Application title</h1>
+        <h1 className="App-title">ch@tter</h1>
       </header>
       <main className="App-main">
-        <TimelineEvent 
-          person={timelineEventData.person} 
-          status={timelineEventData.status} 
-          timeStamp={timelineEventData.timeStamp} />
+        {console.log(timelineData.events)}
+        <Timeline events={timelineData.events} />
       </main>
     </div>
   );
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,21 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
+const Timeline = (props) => {
+  return(
+    <ul className='timeline'>
+      {props.events.map((event, index) => {
+        return(
+          <li key={index}>
+            <TimelineEvent
+              person={event.person}
+              status={event.status}
+              timeStamp={event.timeStamp} />
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default Timeline;
