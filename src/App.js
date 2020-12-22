dc9e7cf55bdb2cb885f2923bff6306e0aef23087
Anna Kim <anna.kim93@gmail.com>
2020-12-21 23:53:29 -0500

    implement timelineEvent component in app using hardcoded data

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
 
 function App() {
-  console.log(timelineData);
+  // console.log(timelineData);
+
+  const timelineEventData = {
+    person: timelineData.events[0].person,
+    status: timelineData.events[0].status,
+    timeStamp: timelineData.events[0].timeStamp
+  }
 
   // Customize the code below
   return (
@@ -14,6 +21,7 @@ function App() {
         <h1 className="App-title">Application title</h1>
       </header>
       <main className="App-main">
+        <TimelineEvent data={timelineEventData} />
       </main>
     </div>
   );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+const TimelineEvent = (props) => {
+  console.log(props)
+  return (
+    <div className='timeline-event'>
+      <div className='event-person'>{props.data.person}</div>
+      <div className='event-status'>{props.data.status}</div>
+      <div className='event-time'>{props.data.timeStamp}</div>
+    </div>
+  );
 }
 
 export default TimelineEvent;
