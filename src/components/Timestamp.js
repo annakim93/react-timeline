2db019d197829d0f4bc39c8056c0e8b1981d4b1c
Anna Kim <anna.kim93@gmail.com>
2020-12-22 16:53:02 -0500

    add proptypes to timestamp component

--- src/components/Timestamp.js
+++ src/components/Timestamp.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import moment from 'moment';
 
 const Timestamp = (props) => {
@@ -9,6 +10,10 @@ const Timestamp = (props) => {
   return (
     <span title={absolute}>{relative}</span>
   );
+};
+
+Timestamp.propTypes = {
+  time: PropTypes.string.isRequired
 };
 
 export default Timestamp;
