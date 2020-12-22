ecaba5949acdf52ab6c473558657286311fc42e8
Anna Kim <anna.kim93@gmail.com>
2020-12-22 00:13:32 -0500

    nested Timestamp component in TimelineEvent component

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -3,12 +3,13 @@ import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
 const TimelineEvent = (props) => {
-  console.log(props)
   return (
     <div className='timeline-event'>
       <div className='event-person'>{props.data.person}</div>
       <div className='event-status'>{props.data.status}</div>
-      <div className='event-time'>{props.data.timeStamp}</div>
+      <div className='event-time'>
+        {<Timestamp timeStamp={props.data.timeStamp} />}
+      </div>
     </div>
   );
 }
