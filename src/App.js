7ddf473f2cd477d181899c29f30f531c4353f2cd
Anna Kim <anna.kim93@gmail.com>
2020-12-22 01:05:27 -0500

    comment cleanup

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
-// import TimelineEvent from './components/TimelineEvent';
 
 function App() {
-  // console.log(timelineData);
-
-  // const timelineEventData = {
-  //   person: timelineData.events[0].person,
-  //   status: timelineData.events[0].status,
-  //   timeStamp: timelineData.events[0].timeStamp
-  // }
-
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="App-title">ch@tter</h1>
       </header>
       <main className="App-main">
-        {console.log(timelineData.events)}
         <Timeline events={timelineData.events} />
       </main>
     </div>
